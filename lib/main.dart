6076929ea96b82caf233caeae4d72ce4c28bcabc
Harry Bryant <harry@fishingfinder.co.uk>
2019-06-18 19:41:44 +0100

    Added TabController

--- lib/main.dart
+++ lib/main.dart
@@ -34,35 +34,73 @@ class MapSampleState extends State<MapSample> {
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
 
+  // @override
+  // Widget build(BuildContext context) {
+  //   return new Scaffold(
+  //     appBar: AppBar(
+  //       title: Text("Fisheri"),
+  //       backgroundColor: Colors.green[700],
+  //       centerTitle: true,
+  //       ),
+  //     body: GoogleMap(
+  //       mapType: MapType.normal,
+  //       initialCameraPosition: _kGooglePlex,
+  //       compassEnabled: false,
+  //       myLocationButtonEnabled: false,
+  //       onMapCreated: (GoogleMapController controller) {
+  //         _controller.complete(controller);
+  //       },
+  //     ),
+  //     floatingActionButton: FloatingActionButton.extended(
+  //       onPressed: _goToTheLake,
+  //       label: Text('To the lake!'),
+  //       icon: Icon(Icons.directions_boat),
+  //       backgroundColor: Colors.green[700],
+  //     ),
+  //     bottomNavigationBar: TabBar(
+  //       tabs: [
+  //         Tab(icon: Icon(Icons.search)),
+  //         Tab(icon: Icon(Icons.account_circle)),
+  //         Tab(icon: Icon(Icons.star))
+  //       ],
+  //     ),
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: AppBar(
-        title: Text("Fisheri"),
-        backgroundColor: Colors.green[700],
-        centerTitle: true,
-        ),
-      body: GoogleMap(
-        mapType: MapType.normal,
-        initialCameraPosition: _kGooglePlex,
-        compassEnabled: false,
-        myLocationButtonEnabled: false,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
-        backgroundColor: Colors.green[700],
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          height: 0,
-          color: Colors.green
-        ),
-      ),
+    return new MaterialApp(
+      home: DefaultTabController(
+          length: 3,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text("Fisheri"),
+              backgroundColor: Colors.green[700],
+              centerTitle: true,
+              bottom: TabBar(
+                tabs: [
+                  Tab(icon: Icon(Icons.search)),
+                  Tab(icon: Icon(Icons.star)),
+                  Tab(icon: Icon(Icons.account_circle)),
+                ],
+              ),
+            ),
+            body: GoogleMap(
+              mapType: MapType.normal,
+              initialCameraPosition: _kGooglePlex,
+              compassEnabled: false,
+              myLocationButtonEnabled: false,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+            ),
+            floatingActionButton: FloatingActionButton.extended(
+              onPressed: _goToTheLake,
+              label: Text('To the lake!'),
+              icon: Icon(Icons.directions_boat),
+              backgroundColor: Colors.green[700],
+            ),
+          )),
     );
   }
 
@@ -125,6 +163,6 @@ class MapSampleState extends State<MapSample> {
 //           },
 //           ),
 //         ),
-        
+
 //       );
 // }
