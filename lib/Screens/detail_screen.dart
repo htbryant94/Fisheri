5b0d77ee80bf2f84f431a237650bbd7c7eef11d5
Harry Bryant <bryant94h@gmail.com>
2019-07-03 20:46:04 +0100

    Added Amenity list widget

--- lib/Screens/detail_screen.dart
+++ lib/Screens/detail_screen.dart
@@ -18,7 +18,7 @@ class DetailScreen extends StatelessWidget {
         _ButtonSection(Colors.blue),
         SizedBox(height: 16),
         _OverviewSection(),
-        _AmenetiesSection(descriptionExpanded),
+        _AmenitiesSection(descriptionExpanded),
       ],
     );
   }
@@ -221,8 +221,22 @@ class _OverviewSection extends StatelessWidget {
   }
 }
 
-class _AmenetiesSection extends StatelessWidget {
-  _AmenetiesSection(this.descriptionExpanded);
+class _Amenity extends StatelessWidget {
+  _Amenity();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(Icons.person),
+        Text('Toilets')
+      ],
+    );
+  }
+}
+
+class _AmenitiesSection extends StatelessWidget {
+  _AmenitiesSection(this.descriptionExpanded);
 
   final bool descriptionExpanded;
 
@@ -230,28 +244,32 @@ class _AmenetiesSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
-        child: Column(
+        child: 
+        Column(children: <Widget>[
+          Text(
+            'Amenities',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          const Padding(padding: EdgeInsets.only(bottom: 16)),
+          Row(
           children: <Widget>[
-            Text(
-              'Ameneties',
-              textAlign: TextAlign.left,
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            Column(
+              children: <Widget>[ 
+                _Amenity(),
+                _Amenity(),
+                _Amenity(),
+              ],
             ),
-            const Padding(padding: EdgeInsets.only(bottom: 16)),
-            Text(
-              'Manor Farm Lakes is an extensive 100 acre fishery based in the heart of Central Bedfordshire, with easy access from the A1. Manor Farm Lakes consists of a range of 7 different fishing lakes with an 18 van touring caravan site with electric hook-ups. A range of fishing experiences are catered for at Manor Farm Lakes including carp fishing for pleasure anglers and specialists, night fishing, fly fishing for carp, match and coarse angling as well as predator spinning, lure and deadbait fishing in the winter months. The River Ivel acts as the boundary along our eastern edge and is also available to fish with a good head of chub, barbel, pike and bream.',
-              softWrap: true,
-              overflow: TextOverflow.fade,
-              maxLines: descriptionExpanded ? 8 : 4,
-            ),
-            FlatButton(
-              onPressed: () {
-                print(descriptionExpanded);
-                // _descriptionExpanded = !_descriptionExpanded;
-              },
-              child: Text("Read More", style: TextStyle(color: Colors.blue)),
-            ),
+            Column(children: <Widget>[
+              _Amenity(),
+              _Amenity(),
+              _Amenity(),
+            ],)
           ],
-        ));
+        )
+        ],
+        )
+        );
   }
 }
+
