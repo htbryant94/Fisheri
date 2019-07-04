8e87659e51d688b0c3031d97a754af5dee6291ad
Harry Bryant <bryant94h@gmail.com>
2019-07-04 20:11:29 +0100

    Added Header widget to detail screen

--- lib/Screens/detail_screen.dart
+++ lib/Screens/detail_screen.dart
@@ -16,13 +16,13 @@ class DetailScreen extends StatelessWidget {
         _DescriptionSection(descriptionExpanded),
         _ButtonSection(Colors.blue),
         SizedBox(height: 16),
-        // _OverviewSection(),
         _FishStocked(),
         _FishingTypes(),
         _AmenitiesSection(),
         _Tickets(),
         _OpeningHours(),
         _FishingRules(true),
+        // _OverviewSection(),
       ],
     );
   }
@@ -41,6 +41,18 @@ class _ImageCarousel extends StatelessWidget {
       height: 240,
       fit: BoxFit.cover,
     );
+  }
+}
+
+class _Header extends StatelessWidget {
+  _Header(this.header);
+
+  final String header;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(header,
+        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold));
   }
 }
 
@@ -127,12 +139,6 @@ class _DescriptionSection extends StatelessWidget {
           // descriptionExpanded = !descriptionExpanded;
         });
 
-    Text header = Text(
-      'Description',
-      textAlign: TextAlign.left,
-      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-    );
-
     Text textBody = Text(
       'Manor Farm Lakes is an extensive 100 acre fishery based in the heart of Central Bedfordshire, with easy access from the A1. Manor Farm Lakes consists of a range of 7 different fishing lakes with an 18 van touring caravan site with electric hook-ups. A range of fishing experiences are catered for at Manor Farm Lakes including carp fishing for pleasure anglers and specialists, night fishing, fly fishing for carp, match and coarse angling as well as predator spinning, lure and deadbait fishing in the winter months. The River Ivel acts as the boundary along our eastern edge and is also available to fish with a good head of chub, barbel, pike and bream.',
       softWrap: true,
@@ -142,9 +148,12 @@ class _DescriptionSection extends StatelessWidget {
 
     return Container(
         padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
-        child: Column(
-          children: [header, SizedBox(height: 16), textBody, readMoreButton],
-        ));
+        child: Column(children: [
+          _Header('Description'),
+          SizedBox(height: 16),
+          textBody,
+          readMoreButton
+        ]));
   }
 }
 
@@ -189,72 +198,69 @@ class _ButtonSection extends StatelessWidget {
   }
 }
 
-class _OverviewSection extends StatelessWidget {
-  _OverviewSection();
+// class _OverviewSection extends StatelessWidget {
+//   _OverviewSection();
 
-  @override
-  Widget build(BuildContext context) {
-    Widget overviewBox = SizedBox(
-      width: 50,
-      height: 50,
-      child: DecoratedBox(decoration: const BoxDecoration(color: Colors.green)),
-    );
+//   @override
+//   Widget build(BuildContext context) {
+//     Widget overviewBox = SizedBox(
+//       width: 50,
+//       height: 50,
+//       child: DecoratedBox(decoration: const BoxDecoration(color: Colors.green)),
+//     );
 
-    Widget overviewRow = Row(
-      children: [overviewBox, overviewBox, overviewBox, overviewBox],
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    );
+//     Widget overviewRow = Row(
+//       children: [overviewBox, overviewBox, overviewBox, overviewBox],
+//       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//     );
 
-    Widget overviewGrid = Column(
-      children: [overviewRow, SizedBox(height: 32), overviewRow],
-    );
+//     Widget overviewGrid = Column(
+//       children: [overviewRow, SizedBox(height: 32), overviewRow],
+//     );
 
-    return Container(
-      padding: const EdgeInsets.fromLTRB(64, 8, 64, 8),
-      child: Column(
-        children: [
-          Text(
-            'Overview',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 16),
-          overviewGrid
-        ],
-      ),
-    );
-  }
-}
+//     return Container(
+//       padding: const EdgeInsets.fromLTRB(64, 8, 64, 8),
+//       child: Column(
+//         children: [
+//           Text(
+//             'Overview',
+//             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+//           ),
+//           const SizedBox(height: 16),
+//           overviewGrid
+//         ],
+//       ),
+//     );
+//   }
+// }
 
 class _FishingTypes extends StatelessWidget {
   _FishingTypes();
 
   @override
   Widget build(BuildContext context) {
-    Widget overviewBox = SizedBox(
+    Widget _box = SizedBox(
       width: 75,
       height: 75,
       child: DecoratedBox(decoration: const BoxDecoration(color: Colors.green)),
     );
 
-    Widget overviewRow = Row(
-      children: [overviewBox, overviewBox, overviewBox],
+    Widget _row = Row(
+      children: [_box, _box, _box],
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
     );
 
-    Widget overviewGrid = Column(
-      children: [overviewRow, SizedBox(height: 16), overviewRow],
+    Widget _grid = Column(
+      children: [_row, SizedBox(height: 16), _row],
     );
 
     return Container(
       padding: const EdgeInsets.fromLTRB(64, 8, 64, 8),
       child: Column(
         children: [
-          Text(
-            'Types of Fishing',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
+          _Header('Types of Fishing'),
           const SizedBox(height: 16),
-          overviewGrid
+          _grid
         ],
       ),
     );
@@ -285,10 +291,7 @@ class _FishStocked extends StatelessWidget {
       padding: const EdgeInsets.fromLTRB(64, 8, 64, 8),
       child: Column(
         children: [
-          Text(
-            'Fish Stocked',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
+          _Header('Fish Stocked'),
           const SizedBox(height: 16),
           overviewGrid
         ],
@@ -317,10 +320,7 @@ class _AmenitiesSection extends StatelessWidget {
         padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
         child: Column(
           children: <Widget>[
-            Text(
-              'Amenities',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
+            _Header('Amenities'),
             const Padding(padding: EdgeInsets.only(bottom: 16)),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -355,10 +355,7 @@ class _Tickets extends StatelessWidget {
         padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
         child: Column(
           children: <Widget>[
-            Text(
-              'Tickets',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
+            _Header('Tickets'),
             SizedBox(height: 8),
             Row(
               children: <Widget>[
@@ -436,11 +433,11 @@ class _OpeningHoursRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget> [
-        Text(day),
-        Text('$openTime - $closeTime'),
-    ]);
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(day),
+          Text('$openTime - $closeTime'),
+        ]);
   }
 }
 
@@ -452,10 +449,7 @@ class _OpeningHours extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
       child: Column(children: <Widget>[
-        Text(
-          'Opening Hours',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-        ),
+        _Header('Opening Hours'),
         SizedBox(height: 16),
         _OpeningHoursRow('Monday', '09:00', '22:00'),
         _OpeningHoursRow('Tuesday', '09:00', '22:00'),
@@ -483,12 +477,6 @@ class _FishingRules extends StatelessWidget {
           // descriptionExpanded = !descriptionExpanded;
         });
 
-    Text header = Text(
-      'Fishing Rules',
-      textAlign: TextAlign.left,
-      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-    );
-
     Text textBody = Text(
       'When and where you can fish National close season rules for coarse fishing apply across the Anglian region. The coarse fishing close season between 15 March and 15 June applies to all rivers, streams, drains, the Norfolk Broads and some stillwaters. Dates are inclusive All dates mentioned in these byelaws are inclusive: this means a stated period, such as 15 March to 15 June, includes the full day of 15 March and the full day of 15 June. Salmon and migratory trout The close season for salmon and migratory trout (in all waters) is 29 September to the last day of February. There is also a weekly close time from 6am on Sunday to midnight on the following day. A small number of rivers in Anglia have been canalised (converted into canals), and remain subject to the national close season: Fossdyke Canal, Louth Canal, Horncastle Canal and the Dilham and North Walsham Canal.',
       softWrap: true,
@@ -498,8 +486,11 @@ class _FishingRules extends StatelessWidget {
 
     return Container(
         padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
-        child: Column(
-          children: [header, SizedBox(height: 16), textBody, readMoreButton],
-        ));
+        child: Column(children: [
+          _Header('Fishing Rules'),
+          SizedBox(height: 16),
+          textBody,
+          readMoreButton
+        ]));
   }
 }
