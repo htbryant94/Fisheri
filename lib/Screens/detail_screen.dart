9a7ffd2e2ba2fe2108e028d20fd63fd861e7dce2
Harry Bryant <bryant94h@gmail.com>
2019-08-25 21:01:08 +0100

    Moved Fishing Rules Section to own file
    # Subject line ~50 characters, no more than 72 characters
    #
    # 72 character wrapped longer description. This should answer:
    #
    # * Why was this change necessary/what was the problem you were addressing
    # * How does your change address the problem?
    # * Are there any side effects or changes that are worth highlighting?
    #
    # Include any relevant links e.g revert pr's, gem pr's, trello tickets

--- lib/Screens/detail_screen.dart
+++ lib/Screens/detail_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fisheri/Screens/detail_screen/fishing_rules_section.dart';
 import 'package:fisheri/house_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:fisheri/models/fish_stock.dart';
@@ -40,54 +41,8 @@ class DetailScreen extends StatelessWidget {
         AmenitiesSection(amenities),
         TicketsSection(),
         OpeningHoursSection(),
-        _FishingRules(true),
+        FishingRulesSection(true),
       ],
     );
-  }
-}
-
-class _Header extends StatelessWidget {
-  _Header(this.header);
-
-  final String header;
-
-  @override
-  Widget build(BuildContext context) {
-    return Align(
-        alignment: Alignment.centerLeft,
-        child: Text(header,
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)));
-  }
-}
-
-class _FishingRules extends StatelessWidget {
-  _FishingRules(this.isExpanded);
-
-  final bool isExpanded;
-
-  @override
-  Widget build(BuildContext context) {
-    FlatButton readMoreButton = FlatButton(
-        child: Text("Read More", style: TextStyle(color: Colors.blue)),
-        onPressed: () {
-          print(isExpanded);
-          // descriptionExpanded = !descriptionExpanded;
-        });
-
-    Text textBody = Text(
-      'When and where you can fish National close season rules for coarse fishing apply across the Anglian region. The coarse fishing close season between 15 March and 15 June applies to all rivers, streams, drains, the Norfolk Broads and some stillwaters. Dates are inclusive All dates mentioned in these byelaws are inclusive: this means a stated period, such as 15 March to 15 June, includes the full day of 15 March and the full day of 15 June. Salmon and migratory trout The close season for salmon and migratory trout (in all waters) is 29 September to the last day of February. There is also a weekly close time from 6am on Sunday to midnight on the following day. A small number of rivers in Anglia have been canalised (converted into canals), and remain subject to the national close season: Fossdyke Canal, Louth Canal, Horncastle Canal and the Dilham and North Walsham Canal.',
-      softWrap: true,
-      overflow: TextOverflow.fade,
-      maxLines: isExpanded ? 8 : 4,
-    );
-
-    return Container(
-        padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
-        child: Column(children: [
-          _Header('Fishing Rules'),
-          SizedBox(height: 16),
-          textBody,
-          readMoreButton
-        ]));
   }
 }
