e9a6b53c144b44de327851e04fd5fef120d8240e
Harry Bryant <bryant94h@gmail.com>
2020-05-04 19:19:03 +0100

    Added MapView & Overview details

--- lib/Screens/detail_screen/description_section.dart
+++ lib/Screens/detail_screen/description_section.dart
@@ -9,13 +9,44 @@ class DescriptionSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Container(
-        padding: const EdgeInsets.all(16),
-        child: Column(children: [
-          HouseTexts.heading('Description'),
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          HouseTexts.heading('Overview'),
           SizedBox(height: 16),
+          Row(children: [
+            Icon(Icons.assignment, color: Colors.green,),
+            SizedBox(width: 8),
+            HouseTexts.body('1,247 catch reports')
+          ]),
+          SizedBox(height: 16),
+          Row(children: [
+            Icon(Icons.calendar_today, color: Colors.red),
+            SizedBox(width: 8),
+            HouseTexts.body('2 upcoming events')
+          ]),
+          SizedBox(height: 16),
+          Row(children: [
+            Icon(Icons.pin_drop, color: Colors.red),
+            SizedBox(width: 8),
+            HouseTexts.body('16 check-ins today'),
+          ]),
+          SizedBox(height: 16),
+          Row(children: [
+            Icon(Icons.star, color: Colors.blue),
+            Icon(Icons.star, color: Colors.blue),
+            Icon(Icons.star, color: Colors.blue),
+            Icon(Icons.star, color: Colors.blue),
+            Icon(Icons.star_half, color: Colors.blue),
+            SizedBox(width: 8),
+            HouseTexts.body('109 reviews')
+          ],
+          ),
+          SizedBox(height: 24),
           HouseTexts.body(text),
-        ]));
+        ],
+      ),
+    );
   }
 }
--- lib/Screens/detail_screen/detail_screen.dart
+++ lib/Screens/detail_screen/detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:fisheri/Screens/detail_screen/fishing_rules_section.dart';
+import 'package:fisheri/house_texts.dart';
+import 'package:fisheri/models/venue_address.dart';
 import 'package:fisheri/models/venue_detailed.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -45,12 +47,13 @@ class DetailScreen extends StatelessWidget {
                 town: venue.address.town,
                 county: venue.address.county,
               ),
-              ButtonSection(Colors.blue),
               DescriptionSection(
                 text: venue.description,
                 descriptionExpanded: descriptionExpanded,
               ),
               SocialMediaSection(social: venue.social),
+              MapViewSection(address: venue.address),
+              ButtonSection(Colors.blue),
               FishStockedSection(venue.fishStocked),
               FishingTypesSection(venue.fishingTypes),
               AmenitiesSection(venue.amenities),
@@ -64,6 +67,75 @@ class DetailScreen extends StatelessWidget {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MapViewSection extends StatelessWidget {
+  MapViewSection({
+    @required this.address
+  });
+
+  final VenueAddress address;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: [
+          HouseTexts.heading('Map View'),
+          SizedBox(height: 16),
+          AspectRatio(
+            aspectRatio: 1.5,
+            child: Card(
+              elevation: 4,
+              child: Stack(
+                children: [
+                  Positioned.fill(
+                    child: Image.asset(
+                      'images/placeholders/lake_map_view.png',
+                      fit: BoxFit.fitWidth,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      width: double.infinity,
+                      height: 75,
+                      color: Colors.white,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                HouseTexts.body(address.street),
+                                HouseTexts.body('${address.town}, ${address.county}'),
+                                HouseTexts.body(address.postcode),
+                              ],
+                            ),
+                            RaisedButton(
+                              color: Colors.blue,
+                              onPressed: () {
+                                print('tapped');
+                              },
+                              child: Text('Directions', style: TextStyle(color: Colors.white),),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
